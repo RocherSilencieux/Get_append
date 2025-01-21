16b580b5661c05bb4141c14155cf5a1e366967c9
Nolent77 <nolhan795@gmail.com>
2025-01-21 15:50:45 +0100

    number of players

--- src/Game.java
+++ src/Game.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 public class Game
 {
     public static void main(String[] args)
     {
-
     }
     public static void setPlayers(int nb, String[][] grid)
     {
+        Scanner scanner = new Scanner(System.in); // To link the user command from the keyboard //
+        System.out.print("➡ Input your number of players : ");
+        nb = scanner.nextInt();
+
         switch(nb)
         {
             case 1:
                 grid[4][5] = "J";
                 break;
             case 2:
+                grid[5][6] = "🟥";
+                grid[5][4] = "🟨";
+                break;
+            case 3:
+                grid[4][5] = "🟩";
+                grid[5][6] = "🟥";
+                grid[5][4] = "🟨";
+                break;
+            case 4:
+                grid[4][6] = "🟧";
+                grid[4][4] = "🟩";
                 grid[5][6] = "🟥";
                 grid[5][4] = "🟨";
                 break;
