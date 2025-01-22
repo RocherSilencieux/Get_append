7fb5f61b4bc9cba7337b468e22459a05cd9a7475
Shado <anais.q33@gmail.com>
2025-01-22 10:55:58 +0100

    Add sorting selection method

--- src/Scores.java
+++ src/Scores.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Scores {
 
@@ -43,6 +44,27 @@ public class Scores {
         }
     }
 
+    public static void chooseSort(List<String[]> players) {
+
+        Scanner scanner = new Scanner(System.in); // To link the user command from the keyboard
+        int sort = scanner.nextInt(); // Read the variable tape by the user
+
+        switch (sort){
+            case 1:
+                // Calls up the descending sorting method
+                descendingSort(players);
+                break;
+            case 2:
+                // Calls up the ascending sorting method
+                ascendingSort(players);
+                break;
+            default: // if no specific cases are found
+                // Calls up the descending sorting method
+                descendingSort(players);
+                break;
+        }
+
+    }
 
     // Displays the top 10 sorted by score
     public static void displayTop10(List<String[]> players) {
@@ -65,13 +87,14 @@ public class Scores {
         players.add(new String[]{"Bob", "150"});
         players.add(new String[]{"Charlie", "90"});
         players.add(new String[]{"Diana", "110"});
-        players.add(new String[]{"Eve", "200"});
+        players.add(new String[]{"Eden", "145"});
         players.add(new String[]{"Frank", "140"});
         players.add(new String[]{"Grace", "95"});
         players.add(new String[]{"Hank", "130"});
-        players.add(new String[]{"Ivy", "180"});
-        players.add(new String[]{"Jack", "170"});
+        players.add(new String[]{"Ivy", "165"});
+        players.add(new String[]{"Jack", "155"});
         players.add(new String[]{"Kevin", "80"});
+        players.add(new String[]{"Eve", "115"});
 
         // Calls up the ascending sorting method
         ascendingSort(players);
