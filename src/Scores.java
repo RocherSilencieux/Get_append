ba26f0bb54bf596c30beb92d8614e9c80efd84cc
Shado <anais.q33@gmail.com>
2025-01-22 14:18:21 +0100

    Update Scores

--- src/Scores.java
+++ src/Scores.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Scores {
 
@@ -44,28 +42,6 @@ public class Scores {
         }
     }
 
-    public static void chooseSort(List<String[]> players) {
-
-        Scanner scanner = new Scanner(System.in); // To link the user command from the keyboard
-        int sort = scanner.nextInt(); // Read the variable tape by the user
-
-        switch (sort){
-            case 1:
-                // Calls up the descending sorting method
-                descendingSort(players);
-                break;
-            case 2:
-                // Calls up the ascending sorting method
-                ascendingSort(players);
-                break;
-            default: // if no specific cases are found
-                // Calls up the descending sorting method
-                descendingSort(players);
-                break;
-        }
-
-    }
-
     // Displays the top 10 sorted by score
     public static void displayTop10(List<String[]> players) {
         // Display a title for the list of top players
@@ -75,33 +51,5 @@ public class Scores {
             // Displays the ranking, the player name and the score
             System.out.println((i + 1) + ". " + players.get(i)[0] + " - " + players.get(i)[1]);
         }
-    }
-
-    public static void main(String[] args) {
-
-        // Création d'une liste de joueurs avec leur nom et score
-        List<String[]> players = new ArrayList<>();
-
-        // Add players and their scores to the list
-        players.add(new String[]{"Alice", "120"});
-        players.add(new String[]{"Bob", "150"});
-        players.add(new String[]{"Charlie", "90"});
-        players.add(new String[]{"Diana", "110"});
-        players.add(new String[]{"Eden", "145"});
-        players.add(new String[]{"Frank", "140"});
-        players.add(new String[]{"Grace", "95"});
-        players.add(new String[]{"Hank", "130"});
-        players.add(new String[]{"Ivy", "165"});
-        players.add(new String[]{"Jack", "155"});
-        players.add(new String[]{"Kevin", "80"});
-        players.add(new String[]{"Eve", "115"});
-
-        // Calls up the ascending sorting method
-        ascendingSort(players);
-        // Calls up the descending sorting method
-        descendingSort(players);
-
-        // Display the top 10 after sorting
-        displayTop10(players);
     }
 }
